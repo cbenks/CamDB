fe46ea19016ae289063e66b84981b1a1cfd3b4fc
Cory <cjbenicak@gmail.com>
2022-09-06 16:09:02 -0400

    added super user

--- cam_project/cam/admin.py
+++ cam_project/cam/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Crypto, Nft
+admin.site.register(User)
+admin.site.register(Crypto)
+admin.site.register(Nft)
 
 # Register your models here.

47aed19e3181fbe2cb3f580534ef6cdd73cbedad
Cory <cjbenicak@gmail.com>
2022-09-06 11:08:42 -0400

    created nft model

--- cam_project/cam/models.py
+++ cam_project/cam/models.py
@@ -16,3 +16,17 @@ class Crypto(models.Model):
         User, on_delete=models.CASCADE, related_name='cryptos')
     name = models.CharField(max_length=50)
     amount = models.PositiveBigIntegerField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Nft(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='nfts')
+    name = models.CharField(max_length=100)
+    blockchain = models.CharField(max_length=50)
+    photo = models.TextField()
+
+    def __str__(self):
+        return self.name

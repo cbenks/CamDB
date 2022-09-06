1528839d7bca2b40f7033d425274649baa0930a4
Cory <cjbenicak@gmail.com>
2022-09-06 16:23:58 -0400

    started making views and urls

--- cam_project/cam/views.py
+++ cam_project/cam/views.py
@@ -1,3 +1,11 @@
+from fcntl import F_DUPFD
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def crypto_list(request):
+    cryptos = Crypto.objects.all().values('user', 'name', 'amount')
+    cryptos_list = list(cryptos)
+    return JsonResponse(cryptos_list, safe=False)
--- cam_project/cam_django/settings.py
+++ cam_project/cam_django/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cam',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}

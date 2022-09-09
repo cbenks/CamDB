dbd6aabc6fec5da5039ff387d846b4bccf0ddeb1
Cory <cjbenicak@gmail.com>
2022-09-09 16:50:48 -0400

    install simplejwt

--- cam_project/cam_django/settings.py
+++ cam_project/cam_django/settings.py
@@ -140,6 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ]
 }
